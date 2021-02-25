ccd7da8a1f3ceef7077a19a9fa864b5ef2825949
Jeong, Kyung Hoon <niezche@hanmail.net>
2021-02-25 12:06:10 +0900

    nested model provider class test

--- ModelFrameWorkTests/ModelProviderFactoryTest.cs
+++ ModelFrameWorkTests/ModelProviderFactoryTest.cs
@@ -8,16 +8,35 @@ namespace Kamu.ModelFrameWorkTests
     [TestClass]
     public class ModelProviderFactoryTest
     {
-        private Uri Name = new Uri("hello://here/");
         private ModelInventory Inventory = new ModelInventory();
 
         [TestMethod]
         public void ShouldBeAbleToOpenAnyProviderWhichRegistersItsSchemeInStaticConstructor()
         {   
+            Uri Name = new Uri("hello://here/");
+
             Assert.IsTrue(Inventory.Open(Name));
 
             Assert.AreNotEqual(0, Kamu.ModelFramework.ModelProviderFactory.Schemes.Count());
 
+            Assert.IsTrue(Kamu.ModelFramework.ModelProviderFactory.Schemes.Contains(Name.Scheme));
+        }
+
+        [TestMethod]
+        public void ShouldRegisterNestedPublicProvider()
+        {
+            Uri Name = new Uri("nested.public://here/");
+
+            Assert.IsTrue(Inventory.Open(Name));
+            Assert.IsTrue(Kamu.ModelFramework.ModelProviderFactory.Schemes.Contains(Name.Scheme));
+        }
+
+        [TestMethod]
+        public void ShouldRegisterNestedPrivateProvider()
+        {
+            Uri Name = new Uri("nested.private://here/");
+
+            Assert.IsTrue(Inventory.Open(Name));
             Assert.IsTrue(Kamu.ModelFramework.ModelProviderFactory.Schemes.Contains(Name.Scheme));
         }
     }
---
+++ ModelFrameWorkTests/Provider/NestProviders.cs
@@ -0,0 +1,82 @@
+using System;
+using Kamu.ModelFramework;
+
+namespace Kamu.ModelFrameworkTests.Provider
+{
+    public class NestProviders
+    {
+        public class NestedPublicProvider : ModelProvider
+        {
+            /// <summary>
+            /// Cliche part
+            /// 1. Set an apropriate name to the static readonly 'Scheme' property
+            /// 2. Define a staic constructor to register a provider factory function
+            /// 3. Define instance constructor to register model factory functions
+            /// </summary>
+
+            #region [Preparation]
+
+            public static string Scheme { get; } = "nested.public";
+
+            static NestedPublicProvider() => Kamu.ModelFramework.ModelProviderFactory.Register(Scheme, (uri, container) => new NestedPublicProvider(uri, container));
+
+            private NestedPublicProvider(Uri uri, ModelContainer container) : base(uri, container)
+            {
+            }
+
+            #endregion
+
+            public override void Save(Model model)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void Update(Model model)
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override void Load(Model model)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private class NestedPrivateProvider : ModelProvider
+        {
+            /// <summary>
+            /// Cliche part
+            /// 1. Set an apropriate name to the static readonly 'Scheme' property
+            /// 2. Define a staic constructor to register a provider factory function
+            /// 3. Define instance constructor to register model factory functions
+            /// </summary>
+
+            #region [Preparation]
+
+            public static string Scheme { get; } = "nested.private";
+
+            static NestedPrivateProvider() => Kamu.ModelFramework.ModelProviderFactory.Register(Scheme, (uri, container) => new NestedPrivateProvider(uri, container));
+
+            private NestedPrivateProvider(Uri uri, ModelContainer container) : base(uri, container)
+            {
+            }
+
+            #endregion
+
+            public override void Save(Model model)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void Update(Model model)
+            {
+                throw new NotImplementedException();
+            }
+
+            protected override void Load(Model model)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
