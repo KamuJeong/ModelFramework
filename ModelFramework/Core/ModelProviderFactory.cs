494dfdafae20c4d13022f173e1cb6e14b9cd70e5
Jeong, Kyung Hoon <niezche@hanmail.net>
2021-02-27 20:25:47 +0900

    Reinforcement of manual provider registration

--- ModelFramework/Core/ModelProviderFactory.cs
+++ ModelFramework/Core/ModelProviderFactory.cs
@@ -11,31 +11,33 @@ namespace Kamu.ModelFramework
 
         public static IEnumerable<string> Schemes => _providerFactory.Keys;
 
-        public static void Register(Type type, bool update = true)
+        public static void Register(Type type)
         {
-            if (type.IsSubclassOf(typeof(ModelProvider)))
-            {
-                var schemeAttribute = type.GetCustomAttribute<SchemeAttribute>(false);
-                
-                if (schemeAttribute == null)
-                    throw new NotImplementedException(nameof(SchemeAttribute));
+            if (!type.IsSubclassOf(typeof(ModelProvider)))
+                throw new ArgumentNullException($"{type.Name} is not a subclass of ModelProvider");
+ 
+            var schemeAttribute = type.GetCustomAttribute<SchemeAttribute>(false);
 
-                if (!update && _providerFactory.ContainsKey(schemeAttribute.Scheme))
-                    throw new ArgumentException($"{schemeAttribute.Scheme} is already registered.");
+            if (schemeAttribute == null)
+                throw new NotImplementedException(nameof(SchemeAttribute));
 
-                var constructorInfo = type.GetConstructor(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic, null,
-                                                        new Type[] { }, null);
-                var uriSetMethod = typeof(ModelProvider).GetProperty("Uri").GetSetMethod(true);
-                var modelsSetMethod = typeof(ModelProvider).GetProperty("Models", BindingFlags.Instance | BindingFlags.NonPublic)
-                                                            .GetSetMethod(true);
-                _providerFactory[schemeAttribute.Scheme] = (u, c) =>
-                    {
-                        var provider = constructorInfo.Invoke(null) as ModelProvider;
-                        uriSetMethod.Invoke(provider, new object[] { u });
-                        modelsSetMethod.Invoke(provider, new object[] { c });
-                        return provider;
-                    };
-            }
+            Register(type, schemeAttribute.Scheme);
+        }
+
+        private static void Register(Type type, string scheme)
+        {
+            var constructorInfo = type.GetConstructor(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic, null,
+                                                    new Type[] { }, null);
+            var uriSetMethod = typeof(ModelProvider).GetProperty("Uri").GetSetMethod(true);
+            var modelsSetMethod = typeof(ModelProvider).GetProperty("Models", BindingFlags.Instance | BindingFlags.NonPublic)
+                                                        .GetSetMethod(true);
+            _providerFactory[scheme] = (u, c) =>
+                {
+                    var provider = constructorInfo.Invoke(null) as ModelProvider;
+                    uriSetMethod.Invoke(provider, new object[] { u });
+                    modelsSetMethod.Invoke(provider, new object[] { c });
+                    return provider;
+                };
         }
 
         public static void Reset() => _providerFactory.Clear();
@@ -54,7 +56,10 @@ namespace Kamu.ModelFramework
                 var schemeAttribute = type.GetCustomAttribute<SchemeAttribute>(false);
                 if (schemeAttribute != null && !schemes.Contains(schemeAttribute.Scheme))
                 {
-                    Register(type, update: false);
+                    if (_providerFactory.ContainsKey(schemeAttribute.Scheme))
+                        throw new ArgumentException($"{schemeAttribute.Scheme} is already registered.");
+
+                    Register(type, schemeAttribute.Scheme);
                 }
             }
 
